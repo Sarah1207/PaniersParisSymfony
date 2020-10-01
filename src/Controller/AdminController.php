8c4b469d3f657f3656d3c9f0f66413279b08e5ff
SARAH <68587805+Sarah1207@users.noreply.github.com>
2020-10-01 11:53:49 +0200

    Update AdminController.php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
 use App\Entity\Paniers;
 use App\Entity\Producteurs;
+use App\Entity\Produits;
 use App\Entity\User;
+use App\Repository\CommandeRepository;
 use App\Repository\PaniersRepository;
 use App\Repository\ProducteursRepository;
+use App\Repository\ProduitsRepository;
+use App\Repository\QuantityCommandRepository;
 use App\Repository\UserRepository;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,34 +22,54 @@ class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
+     * @param UserRepository $userRepository
+     * @param PaniersRepository $paniersRepository
+     * @param ProducteursRepository $producteursRepository
+     * @param ProduitsRepository $produitsRepository
+     * @param CommandeRepository $commandeRepository
+     * @param QuantityCommandRepository $quantityCommandRepository
+     * @return Response
      */
-    public function dashbord(
+
+    public function dashbord (
         UserRepository $userRepository,
         PaniersRepository $paniersRepository,
-        ProducteursRepository $producteursRepository
+        ProducteursRepository $producteursRepository,
+        ProduitsRepository $produitsRepository,
+        CommandeRepository $commandeRepository,
+        QuantityCommandRepository $quantityCommandRepository
     )
+
     {
         //$pdo->query('SELECT * FROM user WHERE id = :id')->fetch()
         $users = $userRepository->findAll();
         $paniers = $paniersRepository->findAll();
+        $produits = $produitsRepository->findAll();
         $producteurs = $producteursRepository->findAll();
+        $commandes = $commandeRepository->findAll();
+        $quantityCommand = $quantityCommandRepository->findAll();
+
         return $this->render('admin/admin.html.twig', [
             'users' => $users,
             'paniers' => $paniers,
+            'produits' =>$produits,
             'producteurs' => $producteurs,
+            'commandes' =>$commandes,
+            'quantityCommand'=>$quantityCommand
         ]);
 
         $em = $this->getDoctrine()->getManager();
         $em->remove($user);
         $em->flush();
+
         return $this->render('admin/admin.html.twig');
-
-
     }
 
+    /* *************************************************************************/
     /**
      * @Route("/admin/producteur/delete", name="admin_producteur_delete")
      *
+     * @param Producteurs $producteur
      * @return Response
      */
     public function deleteProducteur(Producteurs $producteur)
@@ -57,9 +81,11 @@ class AdminController extends AbstractController
 
     }
 
+    /* *************************************************************************/
     /**
      * @Route("/admin/paniers/delete", name="admin_paniers_delete")
      *
+     * @param Paniers $paniers
      * @return Response
      */
     public function deletePanier(Paniers $paniers)
@@ -71,9 +97,11 @@ class AdminController extends AbstractController
 
     }
 
+    /* *************************************************************************/
     /**
      * @Route("/admin/user/delete", name="admin_user_delete")
      *
+     * @param User $user
      * @return Response
      */
     public function deleteUser(User $user)
@@ -83,5 +111,35 @@ class AdminController extends AbstractController
         $em->flush();
         return $this->redirectToRoute('admin');
 
+    }
+
+
+
+    /* *************************************************************************/
+    /**
+     * @Route("/admin/produits/delete", name="admin_produits_delete")
+     * @param Produits $produits
+     * @return Response
+     */
+    public function deleteProduits(Produits $produits)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($produits);
+        $em->flush();
+        return $this->redirectToRoute('admin');
+    }
+
+    /* *************************************************************************/
+    /**
+     * @Route("/admin/commandes/delete", name="admin_commandes_delete")
+     * @param Commande $commandes
+     * @return Response
+     */
+    public function deleteCommande(Commande $commandes)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($commandes);
+        $em->flush();
+        return $this->redirectToRoute('admin');
     }
 }
